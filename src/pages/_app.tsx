5f609a7bc4b96f96a983990ced62f8e5538e655f
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-08-24 02:03:16 +0100

    restructure template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,45 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ChakraProvider, useToast } from "@chakra-ui/react";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const toast = useToast();
+
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error: any, query: any) => {
+        if (
+          query.options.hasOwnProperty("meta") &&
+          query.options.meta.errorMessageToast
+        ) {
+          toast({
+            title: "Server Error",
+            description: `${query.meta.errorMessageToast}`,
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+            position: "top",
+          });
+        } else {
+          toast({
+            title: "Server Error",
+            description: "Fetch Aborted! Check your internet Connection",
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+            position: "top",
+          });
+        }
+      },
+    }),
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
