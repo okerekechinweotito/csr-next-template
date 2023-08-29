be6c1c7d75013fb8138e7f562b12bc22aebe0bb8
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-08-29 10:16:49 +0100

    toast config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,38 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider, useToast } from "@chakra-ui/react";
 import {
+  MutationCache,
   QueryCache,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
   const toast = useToast();
 
   const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+
     queryCache: new QueryCache({
+      onSuccess: (data: any, query: any) => {
+        if (query.options.meta.successMessageToast) {
+          toast({
+            title: "Event Successful",
+            description: `${query.options.meta.successMessageToast}`,
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+            position: "top",
+          });
+        }
+      },
       onError: (error: any, query: any) => {
-        if (
-          query.options.hasOwnProperty("meta") &&
-          query.options.meta.errorMessageToast
-        ) {
+        if (query.options.meta.errorMessageToast) {
           toast({
             title: "Server Error",
             description: `${query.meta.errorMessageToast}`,
@@ -26,10 +41,28 @@ export default function App({ Component, pageProps }: AppProps) {
             isClosable: true,
             position: "top",
           });
-        } else {
+        }
+      },
+    }),
+
+    mutationCache: new MutationCache({
+      onSuccess: (data: any, variables: any, context: any, mutation: any) => {
+        if (mutation.options.meta.successMessageToast) {
+          toast({
+            title: "Event Successful",
+            description: ` ${mutation.options.meta.successMessageToast}`,
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+            position: "top",
+          });
+        }
+      },
+      onError: (data: any, variables: any, context: any, mutation: any) => {
+        if (mutation.options.meta.errorMessageToast) {
           toast({
             title: "Server Error",
-            description: "Fetch Aborted! Check your internet Connection",
+            description: `${mutation.options.meta.errorMessageToast}`,
             status: "error",
             duration: 9000,
             isClosable: true,
